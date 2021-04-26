87d1982e69a35bad81b9b27b18d4e3f254033d00
DarkseidAM <adhish.mathur@gmail.com>
2021-04-26 14:17:24 +0530

    Initial commit

---
+++ app/src/main/java/com/cg/cheapstays/view/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.cg.cheapstays.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cg.cheapstays.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        CoroutineScope(Dispatchers.Main).launch{
+            delay(1000)
+            startActivity(Intent(this@MainActivity,SignUpActivity::class.java))
+            finish()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/cg/cheapstays/view/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.cg.cheapstays.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cg.cheapstays.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
